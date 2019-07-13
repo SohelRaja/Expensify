c61294e7a80c0b6ff75cde76322e10d786d79e05
sohelraja <rohan.nsec@gmail.com>
2019-07-13 21:41:49 +0530

    Linking btw Routes using Link and NavLink

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch} from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
 import './App.css';
 
 const ExpenseDashboardPage = ()=>(
@@ -24,20 +24,32 @@ const HelpExpensePage = ()=>(
 );
 const NotFoundPage = ()=>(
     <div>
-        404!
+        404! - <Link to="/">Go Home</Link>
+    </div>
+);
+const Header = ()=>(
+    <div>
+        <h1>Expensify</h1>
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard </NavLink>
+        <NavLink to="/create" activeClassName="is-active">Create </NavLink>
+        <NavLink to="/edit" activeClassName="is-active">Edit </NavLink>
+        <NavLink to="/help" activeClassName="is-active">Help </NavLink>
     </div>
 );
 
 function App(){
     return(
         <BrowserRouter>
-            <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact/>
-                <Route path="/create" component={AddExpensePage}/>
-                <Route path="/edit" component={EditExpensePage}/>
-                <Route path="/help" component={HelpExpensePage}/>
-                <Route component={NotFoundPage}/>
-            </Switch>
+            <div>
+                <Header />
+                <Switch>
+                    <Route path="/" component={ExpenseDashboardPage} exact/>
+                    <Route path="/create" component={AddExpensePage}/>
+                    <Route path="/edit" component={EditExpensePage}/>
+                    <Route path="/help" component={HelpExpensePage}/>
+                    <Route component={NotFoundPage}/>
+                </Switch>
+            </div>
         </BrowserRouter>
     );
 }
