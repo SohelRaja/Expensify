99059e502579707bb7d20b1063200c99acac5e6c
sohelraja <rohan.nsec@gmail.com>
2019-07-13 21:18:26 +0530

    Added Route and exact

--- src/App.js
+++ src/App.js
@@ -2,10 +2,36 @@ import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom'
 import './App.css';
 
+const ExpenseDashboardPage = ()=>(
+    <div>
+        This is the dashboard page.
+    </div>
+);
+const AddExpensePage = ()=>(
+    <div>
+        This is the Add Expense page.
+    </div>
+);
+const EditExpensePage = ()=>(
+    <div>
+        This is the Edit Expense page.
+    </div>
+);
+const HelpExpensePage = ()=>(
+    <div>
+        This is the Help Expense page.
+    </div>
+);
+
 function App(){
     return(
         <BrowserRouter>
-            
+            <div>
+                <Route path="/" component={ExpenseDashboardPage} exact/>
+                <Route path="/create" component={AddExpensePage}/>
+                <Route path="/edit" component={EditExpensePage}/>
+                <Route path="/help" component={HelpExpensePage}/>
+            </div>
         </BrowserRouter>
     );
 }
