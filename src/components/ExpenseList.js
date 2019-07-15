7dd6d4613ac2d230f52dec831f9aaeefa7400993
sohelraja <rohan.nsec@gmail.com>
2019-07-15 20:15:35 +0530

    Connecting store to component using connect from react-redux package

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,10 +1,20 @@
 import React from 'react';
-const ExpenseList = (()=>{
+import { connect } from 'react-redux';
+const ExpenseList = ((props)=>{
     return (
         <div>
             <h1>Expense List</h1>
+            {props.expenses.length}
+            {props.filters.text}
         </div>
     );
 });
 
-export default ExpenseList;
+const ConnectedExpenseList = connect((state)=>{
+    return {
+        expenses: state.expenses,
+        filters: state.filters
+    };
+})(ExpenseList);
+
+export default ConnectedExpenseList;
