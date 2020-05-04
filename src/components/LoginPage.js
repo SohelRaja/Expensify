444f608ae21f534385bb88eeaf1414251c50e0b5
SohelRaja <rohan.nsec@gmail.com>
2020-05-04 13:51:58 +0530

    Login Page Style added

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Button} from 'reactstrap';
+
 import {startLogin} from '../actions/auth';
 
 export const LoginPage = ({startLogin}) => (
-    <div>
-        <button onClick={startLogin}>Login</button>
+    <div className="box-layout">
+        <div className="box-layout-box">
+            <h1 className="box-layout-title">Expensify</h1>
+            <p>It's time to get your expenses under control.</p>
+            <Button onClick={startLogin} color="primary" size="lg">Login with Google</Button>
+        </div>
     </div>
 );
 
